1abc3b99a7108c0255d3fdb9d96c6651a1634b08
Stephen Weigel <stephen@stephenweigel.com>
2020-04-07 14:08:04 -0500

    Switch to using react-sagas

--- src/App.js
+++ src/App.js
@@ -11,38 +11,17 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-// import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
+import { checkUserSession } from './redux/user/user.actions';
+
 class App extends React.Component {
 
-
     unsubscribeFromAuth = null;
-    
+
     componentDidMount() {
-        const { setCurrentUser } = this.props;
-
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-            if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth);
-
-                userRef.onSnapshot(snapshot => {
-                    setCurrentUser({
-                            id: snapshot.id,
-                            ...snapshot.data()
-                        });
-                });
-            } else {
-                setCurrentUser(userAuth); // null
-            }
-        });
+        const { checkUserSession } = this.props;
+        checkUserSession();
     }
-
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
-
 
     render() {
         return (
@@ -64,7 +43,7 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
-});
+    checkUserSession: () => dispatch(checkUserSession())
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -19,17 +21,10 @@ class SignIn extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
-
+        const { emailSignInStart } = this.props;
         const { email, password } = this.state;
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
-        } catch (err) {
-            console.log('error signing in: ', err.message);
-        }
-
-        
+        emailSignInStart(email, password);
     }
 
     handleChange = event => {
@@ -38,6 +33,8 @@ class SignIn extends React.Component {
     }
 
     render() {
+        const { googleSignInStart } = this.props;
+
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -63,7 +60,7 @@ class SignIn extends React.Component {
 
                     <div className='buttons'>
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                        <CustomButton type="button" onClick={googleSignInStart} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
@@ -71,5 +68,10 @@ class SignIn extends React.Component {
     }
 }
 
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+});
 
-export default SignIn;
+
+export default connect(null, mapDispatchToProps)(SignIn);
